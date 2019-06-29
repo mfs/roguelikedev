7f084676a7ea91923f7c18daff1b13a6bf6d70d0
Mike Sampson <mike@sambodata.com>
2019-06-29 22:28:41 +1000

    Start of entity system.

---
+++ src/entity.rs
@@ -0,0 +1,20 @@
+use termion::color;
+
+pub struct Entity {
+    pub x: i64,
+    pub y: i64,
+    pub c: char,
+    pub color: color::Rgb
+}
+
+impl Entity {
+    pub fn new(x: i64, y: i64, c: char, color: color::Rgb) -> Self {
+        Self { x, y, c, color}
+    }
+
+    pub fn mov(&mut self, dx: i64, dy: i64) {
+        self.x += dx;
+        self.y += dy;
+    }
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 extern crate failure;
 extern crate termion;
 
+mod entity;
+
 use failure::Error;
 use std::io::{stdin, stdout, Write};
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 use termion::{clear, cursor, color,style};
+
+use entity::Entity;
 
 fn main() -> Result<(), Error> {
     let stdin = stdin();
@@ -15,31 +19,31 @@ fn main() -> Result<(), Error> {
     let screen_w = 80;
     let screen_h = 50;
 
-    let mut player_x = screen_w / 2;
-    let mut player_y = screen_h / 2;
+    let mut player = Entity::new(screen_w / 2, screen_h / 2, '@', color::Rgb(0x0, 0x95, 0xff));
+    let mut npc = Entity::new(screen_w / 2 - 5, screen_h / 2, '@', color::Rgb(0x0, 0x00, 0xff));
 
     writeln!(stdout, "{}{}", clear::All, cursor::Hide)?;
 
     writeln!(stdout, "{}", color::Fg(color::Rgb(0x0, 0x95, 0xff)))?;
 
-    writeln!(stdout, "{}@", cursor::Goto(player_x, player_y))?;
+    writeln!(stdout, "{}@", cursor::Goto(player.x as u16, player.y as u16))?;
     stdout.flush()?;
 
     for c in stdin.keys() {
         // clear
-        writeln!(stdout, "{} ", cursor::Goto(player_x, player_y))?;
+        writeln!(stdout, "{} ", cursor::Goto(player.x as u16, player.y as u16))?;
         // input
         match c? {
-            Key::Char('h') => player_x -= 1,
-            Key::Char('j') => player_y += 1,
-            Key::Char('k') => player_y -= 1,
-            Key::Char('l') => player_x += 1,
+            Key::Char('h') => player.x -= 1,
+            Key::Char('j') => player.y += 1,
+            Key::Char('k') => player.y -= 1,
+            Key::Char('l') => player.x += 1,
             Key::Char('q') => break,
             _ => {}
         }
 
         // render
-        writeln!(stdout, "{}@", cursor::Goto(player_x, player_y))?;
+        writeln!(stdout, "{}@", cursor::Goto(player.x as u16, player.y as u16))?;
         stdout.flush()?;
     }
 
